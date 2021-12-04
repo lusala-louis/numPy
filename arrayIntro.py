41b45a6846ab8528fb78c56110094c52adf67423
lusala-louis <lusalalouis@gmail.com>
2021-12-05 01:17:07 +0300

    First commit

---
+++ arrayIntro.py
@@ -0,0 +1,12 @@
+import numpy as np
+# Create a rank 1 array
+a = np.array([0, 1, 2])
+print (type(a))
+# this will print the dimension of the array
+print (a.shape)
+print (a[0])
+print (a[1])
+print (a[2])
+# Change an element of the array
+a[0] = 5
+print (a)
