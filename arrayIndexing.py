1f306f78c341d28607cc453de6b5adb7a8a6dcac
lusala-louis <lusalalouis@gmail.com>
2021-12-09 11:27:22 +0300

    Basic array commands in numPy

--- arrayIndexing.py
+++ arrayIndexing.py
@@ -2,12 +2,17 @@
 import numpy as np
 a=np.array([[1,2],[3,4]])
 print(a)
-print(a[[0,1],[0,1]])
 #Getting the dimension of an array
 print(a.ndim)
 #Get the shape of the array
 print(a.shape)
 #Get the datatype of the array
 print(a.dtype)
-b=np.array([[1,3],[3,5]])
-print(np.array([b[0,1],b[1,1]]))
+#Get the size of the array
+print(a.itemsize)
+#Get the number of items in  the array
+print(a.size)
+#Get the size of the array
+print(a.size*a.itemsize)
+print(a.nbytes)
+print(a[[0,1],[0,1]])
