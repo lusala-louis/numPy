effaf8833e72adc2d19e69414ccfa568820ebcf5
lusala-louis <lusalalouis@gmail.com>
2021-12-09 12:24:42 +0300

    Additional code

--- arrayAccess.py
+++ arrayAccess.py
@@ -1,3 +1,20 @@
 import numpy as np
 array1=np.array([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]])
 print(array1)
+print(' ')
+#Get a specific element [r,c]
+print(array1[1,5])
+print(' ')
+#Get a specific row
+print(array1[0, : ])
+print(' ')
+#Get a specific column
+print(array1[: , 2 ])
+print(' ')
+#stepping through a row[startindex:stopindex:stepindex]
+print(array1[0,1:6:2])
+print(' ')
+#Changing an element
+array1[1,5]=20
+print(array1)
+print(' ')
