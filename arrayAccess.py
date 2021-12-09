25c68dee3516e697bbbc7af3777a406fad528c7c
lusala-louis <lusalalouis@gmail.com>
2021-12-09 12:45:33 +0300

    Added the3D matrix

--- arrayAccess.py
+++ arrayAccess.py
@@ -18,3 +18,18 @@ print(' ')
 array1[1,5]=20
 print(array1)
 print(' ')
+#Changing a series of elements in a column
+array1[:,2]=5
+print(array1)
+print(' ')
+array1[:,2]=[2,5]
+print(array1)
+print(' ')
+#Working on a 3d array
+array2=np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+print(array2)
+print(' ')
+array2[0,1,1]=6  #Changing the '4' in [3,4] to '6'
+print(array2)
+print(' ')
+print(array2[:,0,:])   #Getting the elements in the first rows of the two matrices
