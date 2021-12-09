68e461be8c6f7c6aa8a2af111cef885dbda61570
lusala-louis <lusalalouis@gmail.com>
2021-12-09 21:43:01 +0300

    Basic math operations of matrices in numPy

---
+++ numpy_mathematics.py
@@ -0,0 +1,28 @@
+import numpy as np
+array1=np.array([1,2,3,4])
+print(array1)
+print(' ')
+#Summation
+print(array1+2)
+print(' ')
+#Subtraction
+print(array1-2)
+print(' ')
+#Multiplication and power
+print(array1*2)
+print(array1**2)
+print(' ')
+#Division
+array2=np.array([0,2,4,6])
+print(array2/2)
+print(' ')
+#Trigonometry
+array1_sine=np.sin(array1)
+print(array1_sine)
+print(' ')
+array1_cos=np.cos(array1)
+print(array1_cos)
+print(' ')
+array1_tan=np.tan(array1)
+print(array1_tan)
+print(' ')
